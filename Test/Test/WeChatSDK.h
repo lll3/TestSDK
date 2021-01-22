6c66a2616b1639acbd91cc33165b9840c1586737
lll3 <467445562@qq.com>
2021-01-22 17:44:01 +0800

    新增项目

---
+++ Test/Test/WeChatSDK.h
@@ -0,0 +1,16 @@
+//
+//  WeChatSDK.h
+//  Test
+//
+//  Created by myzx on 2021/1/19.
+//  Copyright © 2021 myzx. All rights reserved.
+//
+
+#ifndef WeChatSDK_h
+#define WeChatSDK_h
+
+#import "WechatPay.h"
+
+#endif /* WeChatSDK_h */
+
+
---
+++ Test/Test/WechatPay.m
@@ -0,0 +1,19 @@
+//
+//  WechatPay.m
+//  Test
+//
+//  Created by myzx on 2021/1/19.
+//  Copyright © 2021 myzx. All rights reserved.
+//
+
+#import "WechatPay.h"
+
+@implementation WechatPay
+
+- (void)play:(NSInteger)money{
+    NSLog(@"sdadas");
+}
+
+
+
+@end
